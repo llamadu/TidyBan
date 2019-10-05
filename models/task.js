88c165d3984e29486a8fc7559361fa76ce09f494
Scarlett <jenny_lieseke@hotmail.com>
2019-10-05 10:35:25 -0700

    models

---
+++ models/task.js
@@ -0,0 +1,8 @@
+  
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define("Task", {
+      name: DataTypes.STRING,
+      deadline: DataTypes.DATE,
+      importance: DataTypes.INTEGER
+    })
+  }
