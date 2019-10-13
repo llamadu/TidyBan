ffdb30238f3f8e67283fdc0fccf691ab4751b4e2
Scarlett <jenny_lieseke@hotmail.com>
2019-10-13 13:17:02 -0700

    Rough bootstrap adds

--- models/helper.js
+++ models/helper.js
@@ -2,7 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   var Helper = sequelize.define('Helper', {
     name: DataTypes.STRING
-  });
+    allowNull: false,
+      validate:{
+          len:[20]
+  };
+
+  Helper.associate = function(models) {
+    models.Helper.hasMany(models.Task),
+    onDelete: "cascade";
 
   return Helper;
 };
